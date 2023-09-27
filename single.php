68770376a5d3d16a1cdf31f217bb1f01e65c6746
k-shibaike <130716259+k-shibaike@users.noreply.github.com>
2023-09-27 13:56:19 +0900

    Update single.php

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <!-- 記事ページ -->
-<div class="main">
+<div id="single_main">
   <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
       <!-- 日付 -->
